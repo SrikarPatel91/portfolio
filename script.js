cb15cf32cabfb6a0d6b743295c30e89ae05e98c4
B.Srikar <srikarpatel9177@gmail.com>
2024-12-29 19:19:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const targetId = this.getAttribute('href');
+    const targetSection = document.querySelector(targetId);
+    if (targetSection) {
+        targetSection.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+        });
+    }
+    });
+});
+    
